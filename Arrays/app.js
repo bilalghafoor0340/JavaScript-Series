8044077eb278f78df16fa9ff7cafbb1bb7a39f04
Bilal Ghafoor <bilalghafoor1020@gmail.com>
2026-06-05 19:08:34 +0500

    Initial commit

---
+++ Arrays/app.js
@@ -0,0 +1,40 @@
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+let cities = ["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata"]
+
+let myArr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// console.log(myArr[2]);
+
+// myArr.push(11)
+// myArr.push(12)
+// myArr.unshift(0)
+// myArr.unshift(3)
+// myArr.unshift(1)
+// myArr.pop()
+// myArr.shift()
+// myArr.shift()
+// myArr.shift(6)
+// myArr.pop()
+// myArr.includes(7)
+
+// console.log(myArr.includes(7))
+// console.log(myArr.indexOf(10))
+// console.log(cities)
+// console.log(myArr2)
+// let newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+console.log("A", myArr)
+let arrSlice  = myArr.slice(1, 4)
+console.log(arrSlice)
+
+console.log("B ", myArr)
+
+let arrSplice = myArr.splice(1,4)
+console.log(arrSplice)
+
+console.log("C", myArr)
